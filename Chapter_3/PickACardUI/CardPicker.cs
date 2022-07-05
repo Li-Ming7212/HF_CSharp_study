c67b232209d9c0412197b934a6d698269b15dad3
liming_7212 <281038535@qq.com>
2022-07-05 16:19:13 +0800

    Chapter_1_2_3

---
+++ Chapter_3/PickACardUI/CardPicker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardUI {
+    internal class CardPicker {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards) {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++) {
+                pickedCards[i] = RandomSuit() + " " + RandomValue();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit() {
+            int value = random.Next(1, 5);
+            if (value == 1) return "黑桃";
+            if (value == 2) return "红心";
+            if (value == 3) return "梅花";
+            return "方块";
+        }
+
+        private static string RandomValue() {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
---
+++ Chapter_3/PickRandomCards/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PickRandomCards {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("你要几张牌？张数: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards)) {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards)) {
+                    Console.WriteLine(card);
+                }
+            }
+            else {
+                Console.WriteLine("请输入正确的数字.");
+            }
+        }
+    }
+}
+
+
+
+
+
