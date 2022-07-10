8c27ec528aa5d5d2608bc7478ecfb9020f6f4af1
liming_7212 <281038535@qq.com>
2022-07-10 20:20:03 +0800

    Chapter_5

---
+++ Chapter_5/HiLo/HiLo/HiLoGame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HiLo
+{
+    /*
+     * In some early versions of Head First C# (4th edition) there was a minor technical issue
+     * with this exercise. This code corresponds to the updated version, which you can find here:
+     * https://github.com/head-first-csharp/fourth-edition/raw/master/Additional_material/Corrected_pages_243_and_244.pdf
+     */
+
+    static class HiLoGame
+    {
+        public const int MAXIMUM = 10;
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, MAXIMUM + 1);
+        private static int nextNumber = random.Next(1, MAXIMUM + 1);
+        private static int pot = 10;
+
+        public static int GetPot() { return pot; }
+
+        public static void Guess(bool higher)
+        {
+            if ((higher && nextNumber >= currentNumber) ||
+                    (!higher && nextNumber <= currentNumber))
+            {
+                Console.WriteLine("You guessed right!");
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Bad luck, you guessed wrong.");
+                pot--;
+            }
+            currentNumber = nextNumber;
+            nextNumber = random.Next(1, MAXIMUM + 1);
+            Console.WriteLine($"The current number is {currentNumber}");
+        }
+
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (nextNumber >= half)
+                Console.WriteLine($"The current number is {currentNumber}, the next number is at least {half}");
+            else Console.WriteLine($"The current number is {currentNumber}, the next is at most {half}");
+            pot--;
+        }
+    }
+
+}
---
+++ Chapter_5/SwordDamage/SwordDamage_WPF.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SwordDamage {
+    class SwordDamage_WPF {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Damage { get; private set; }
+
+        private int roll;
+        public int Roll {
+            get { return roll; }
+            set {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool magic;
+        public bool Magic {
+            get { return magic; }
+            set {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+        public bool Flaming {
+            get { return flaming; }
+            set {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        
+        int flamingDamage = 0;
+
+        public void CalculateDamage() {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+
+            //Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+            //Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+        }
+
+
+
+        public SwordDamage_WPF(int startingRoll) {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+    }
+}
