fb39666fefb95e874f8dcaa247ce9c22b4c62e39
liming_7212 <281038535@qq.com>
2022-07-19 14:56:07 +0800

    Chapter_7

---
+++ Chapter_7/BeehiveManagementSystem/HiveDefender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem {
+    internal class HiveDefender : Bee, IDefender {
+
+        public override float CostPerShift { get { return 1.7f; } }
+        public HiveDefender(string job) : base("Hive Defender") { }
+
+        protected override void DoJob() {
+            throw new NotImplementedException();
+        }
+
+        public void Defend() {
+
+        }
+    }
+}
---
+++ Chapter_7/HippoAndWolf/HippoAndWolf/Canine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HippoAndWolf {
+    abstract class Canine : Animal//犬科
+    {
+        public bool BelongsToPack { get; protected set; } = false;
+    }
+}
