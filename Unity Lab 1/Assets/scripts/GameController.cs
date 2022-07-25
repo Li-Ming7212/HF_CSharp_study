4045aa08e67ff061c3b03f6ab73f46b5d91c6387
liming_7212 <281038535@qq.com>
2022-07-25 16:53:11 +0800

    Unity_Lab_4

--- Unity Lab 1/Assets/scripts/GameController.cs
+++ Unity Lab 1/Assets/scripts/GameController.cs
@@ -1,23 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
     public GameObject 一号球预制体;
-    static int BallCount = 0;
-    public int BallNumber;
+    public Text ScoreText;
+    public Button PlayAgainButton;
+
+
+    public int Score = 0;
+    //static int BallCount = 0;
+    //public int BallNumber;
+
+    public bool GameOver = false;
+
+    public int NumberOfBalls = 0;
+    public int MaximumBalls = 15;
 
     void Start() {
         InvokeRepeating("AddABall", 1.5f, 1);
 
-        BallCount++;
-        BallNumber = BallCount;
+        //BallCount++;
+        //BallNumber = BallCount;
     }
 
-    void Update() { }
+    void Update() {
+        ScoreText.text = Score.ToString();
+    }
 
     void AddABall() {
-        Instantiate(一号球预制体);
+        if (!GameOver) {
+            Instantiate(一号球预制体);
+            NumberOfBalls++;
+            if (NumberOfBalls >= MaximumBalls) {
+                GameOver = true;
+                PlayAgainButton.gameObject.SetActive(true);
+            }
+        }
+
+    }
+
+    public void ClickedOnBall() {
+        Score++;
+        NumberOfBalls--;
+    }
+
+    public void StartGame() {
+        foreach (GameObject ball in GameObject.FindGameObjectsWithTag("GameController")) {
+            Destroy(ball);
+        }
+        PlayAgainButton.gameObject.SetActive(false);
+        Score = 0;
+        NumberOfBalls = 0;
+        GameOver = false;
     }
 }
--- Unity Lab 1/Assets/scripts/OneBallBehaviour.cs
+++ Unity Lab 1/Assets/scripts/OneBallBehaviour.cs
@@ -5,18 +5,48 @@ using UnityEngine;
 public class OneBallBehaviour : MonoBehaviour {
     //unity lab 3 and 4 scripts
 
-    public float XRotation = 0;
-    public float YRotation = 1;
-    public float ZRotation = 0;
-    public float DegreesPerSecond = 180;
+    public float XSpeed;
+    public float YSpeed;
+    public float ZSpeed;
+    public float Multiplier = 0.75f;
+    public float TooFar = 5;
+
+    static int BallCount = 0;
+    public int BallNumber;
 
     void Start() {
-        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+
+        BallCount++;
+        BallNumber = BallCount;
+        //transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+        ResetBall();
     }
 
     void Update() {
-        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
-        transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
-        //Debug.DrawRay(Vector3.zero, axis, Color.yellow, .5f);
+        transform.Translate(Time.deltaTime * XSpeed, Time.deltaTime * YSpeed, Time.deltaTime * ZSpeed);
+
+        XSpeed += Multiplier - Random.value * Multiplier * 2;
+        YSpeed += Multiplier - Random.value * Multiplier * 2;
+        ZSpeed += Multiplier - Random.value * Multiplier * 2;
+
+        if ((Mathf.Abs(transform.position.x) > TooFar) || (Mathf.Abs(transform.position.y) > TooFar) || (Mathf.Abs(transform.position.z) > TooFar)) {
+            ResetBall();
+        }
+    }
+
+    private void OnMouseDown() {
+        GameController controller = Camera.main.GetComponent<GameController>();
+        if (!controller.GameOver) {
+            controller.ClickedOnBall();
+            Destroy(gameObject);
+        }
+    }
+
+    private void ResetBall() {
+        XSpeed += Multiplier - Random.value * Multiplier * 2;
+        YSpeed += Multiplier - Random.value * Multiplier * 2;
+        ZSpeed += Multiplier - Random.value * Multiplier * 2;
+
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
     }
 }
